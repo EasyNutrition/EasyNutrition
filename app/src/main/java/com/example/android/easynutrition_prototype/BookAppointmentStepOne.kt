dc1f41817edcbf2dda445b123c802f956e1bb08c
Antonio Ferrándiz <antoniojfb01@gmail.com>
2022-05-06 16:13:52 -0500

    adding interaction

---
+++ app/src/main/java/com/example/android/easynutrition_prototype/BookAppointmentStepOne.kt
@@ -0,0 +1,11 @@
+package com.example.android.easynutrition_prototype
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookAppointmentStepOne : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_appointment_step_one)
+    }
+}
---
+++ app/src/main/java/com/example/android/easynutrition_prototype/PatientHomeView.kt
@@ -0,0 +1,11 @@
+package com.example.android.easynutrition_prototype
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PatientHomeView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient_home_view)
+    }
+}

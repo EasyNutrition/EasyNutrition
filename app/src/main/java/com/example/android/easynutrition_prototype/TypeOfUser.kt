995e04a2db3907cd1f72c5954c7f5651385567fb
Antonio Ferrándiz <antoniojfb01@gmail.com>
2022-05-06 15:04:01 -0500

    type of user view

---
+++ app/src/main/java/com/example/android/easynutrition_prototype/TypeOfUser.kt
@@ -0,0 +1,11 @@
+package com.example.android.easynutrition_prototype
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TypeOfUser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_type_of_user)
+    }
+}

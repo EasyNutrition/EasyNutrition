392475a9cf957e876c81c10ae0172952d375b58b
Gianella_Coronel <u201919040@upc.edu.pe>
2022-05-05 21:44:34 -0500

    Regitro Nutricionista

---
+++ app/src/main/java/com/example/android/easynutrition_prototype/RegisterStep3.kt
@@ -0,0 +1,11 @@
+package com.example.android.easynutrition_prototype
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterStep3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_step3)
+    }
+}
---
+++ app/src/main/java/com/example/android/easynutrition_prototype/RegisterStepOptional.kt
@@ -0,0 +1,11 @@
+package com.example.android.easynutrition_prototype
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterStepOptional : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_step_optional)
+    }
+}
